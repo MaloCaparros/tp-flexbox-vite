9491072ff906734b359faf09997c8949267b355d
Malo Caparros <malo.caparros@etu.univ-poitiers.fr>
2024-06-12 14:23:18 +0200

    avancé

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,15 @@
-
 const water = document.getElementById("water");
-water.style = "height: 10em; -webkit-transition: all 15s ease-out; -moz-transition: all 15s ease-out; -o-transition: all 15s ease-out; transition: all 15s ease-out;";
-       
+water.style =
+  "height: 10em; -webkit-transition: all 15s ease-out; -moz-transition: all 15s ease-out; -o-transition: all 15s ease-out; transition: all 15s ease-out;";
+
+function vibrateSimple() {
+  navigator.vibrate(200);
+}
+
+function vibratePattern() {
+  navigator.vibrate([100, 200, 200, 200, 500]);
+}
+
+sensor = new Accelerometer();
+sensor.addEventListener('reading', listener);
+sensor.start();
