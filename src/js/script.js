9f854a79ce07059111fcd90164ed260971663cbc
Malo Caparros <malo.caparros@etu.univ-poitiers.fr>
2024-06-17 08:41:13 +0200

    truc

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,23 @@
-const water = document.getElementById("water");
-water.style =
-  "height: 10em; -webkit-transition: all 15s ease-out; -moz-transition: all 15s ease-out; -o-transition: all 15s ease-out; transition: all 15s ease-out;";
-// Initialisation du capteur
-let sensor;
+window.addEventListener("DOMContentLoaded", function () {
+  const water = document.getElementById("water");
+  let sensor;
 
-if ('Accelerometer' in window) {
+  water.style =
+    "height: 10em; -webkit-transition: all 15s ease-out; -moz-transition: all 15s ease-out; -o-transition: all 15s ease-out; transition: all 15s ease-out;";
+  // Initialisation du capteur
+
   sensor = new Accelerometer();
-  sensor.addEventListener('reading', () => {
+  sensor.start();
+  sensor.addEventListener("reading", () => {
     console.log(`Acceleration along X-axis ${sensor.x}`);
     console.log(`Acceleration along Y-axis ${sensor.y}`);
     console.log(`Acceleration along Z-axis ${sensor.z}`);
   });
-}
 
-document.getElementById('playButton').addEventListener('click', function(event) {
-  event.preventDefault();
-
-  // Démarrer le capteur de l'accéléromètre
-  if (sensor) {
-    sensor.start();
-  }
-
-  // Démarrer le compteur d'une minute
-  let timerElement = document.getElementById('timer');
-  let timeLeft = 60;
+  let timerElement = document.getElementById("timer");
+  let scoreELement = document.getElementById("score");
+  scoreELement.textContent = sensor.x + " " + sensor.y + " " + sensor.z;
+  let timeLeft = 30;
   timerElement.textContent = timeLeft;
 
   let countdown = setInterval(() => {
@@ -35,7 +29,7 @@ document.getElementById('playButton').addEventListener('click', function(event) 
       if (sensor) {
         sensor.stop();
       }
-      alert('Temps écoulé !');
+      alert("Temps écoulé !");
     }
   }, 1000);
 });
