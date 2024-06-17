dfad529e6a3e2be0cd9815eab2b19db4f5a5286f
Malo Caparros <malo.caparros@etu.univ-poitiers.fr>
2024-06-17 09:00:28 +0200

    terst

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,48 @@
-window.addEventListener("DOMContentLoaded", function () {
- /* const water = document.getElementById("water");
+window.addEventListener('DOMContentLoaded', function () {
+  const timerElement = document.getElementById("timer");
+  const scoreElement = document.getElementById("moAccel");
   let sensor;
+  let countdown;
 
-  water.style =
-    "height: 10em; -webkit-transition: all 15s ease-out; -moz-transition: all 15s ease-out; -o-transition: all 15s ease-out; transition: all 15s ease-out;";
-  // Initialisation du capteur
+  if ('LinearAccelerationSensor' in window) {
 
-  sensor = new Accelerometer();
-  sensor.start();
-  sensor.addEventListener("reading", () => {
-    console.log(`Acceleration along X-axis ${sensor.x}`);
-    console.log(`Acceleration along Y-axis ${sensor.y}`);
-    console.log(`Acceleration along Z-axis ${sensor.z}`);
-  });
+    let lastReadingTimestamp;
+    sensor = new LinearAccelerationSensor({ frequency: 60 });
+    sensor.addEventListener('reading', () => {
+      if (lastReadingTimestamp) {
+        intervalHandler(Math.round(sensor.timestamp - lastReadingTimestamp));
+      }
+      lastReadingTimestamp = sensor.timestamp;
+      accelerationHandler(sensor.y, 'moAccel');
+    });
+    sensor.start();
 
-  let timerElement = document.getElementById("timer");
-  let scoreELement = document.getElementById("score");
-  scoreELement.textContent = "X:" + sensor.x + "Y:" +  sensor.y + "Z:" + sensor.z;
+  } else if ('DeviceMotionEvent' in window) {
+
+    var onDeviceMotion = function (eventData) {
+      accelerationHandler(eventData.acceleration.y, 'moAccel');
+      intervalHandler(eventData.interval);
+    }
+
+    window.addEventListener('devicemotion', onDeviceMotion, false);
+  } else {
+    document.getElementById('moApi').innerHTML = 'No Accelerometer API available';
+  }
+
+  function accelerationHandler(y, targetId) {
+    var info = `Y: ${y !== null ? y.toFixed(3) : 'N/A'}`;
+    document.getElementById(targetId).innerHTML = info;
+  }
+
+  function intervalHandler(interval) {
+    document.getElementById("moInterval").innerHTML = interval;
+  }
+
+  // Démarrer le compteur d'une minute
   let timeLeft = 30;
   timerElement.textContent = timeLeft;
 
-  let countdown = setInterval(() => {
+  countdown = setInterval(() => {
     timeLeft--;
     timerElement.textContent = timeLeft;
 
@@ -31,69 +53,5 @@ window.addEventListener("DOMContentLoaded", function () {
       }
       alert("Temps écoulé !");
     }
-  }, 1000);*/
-  if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
-    document.getElementById('moApi').innerHTML = 'Generic Sensor API';
-    
-    let lastReadingTimestamp;
-    let accelerometer = new LinearAccelerationSensor();
-    accelerometer.addEventListener('reading', e => {
-      if (lastReadingTimestamp) {
-        intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
-      }
-      lastReadingTimestamp = accelerometer.timestamp
-      accelerationHandler(accelerometer, 'moAccel');
-    });
-    accelerometer.start();
-    
-    if ('GravitySensor' in window) {
-      let gravity = new GravitySensor();
-      gravity.addEventListener('reading', e => accelerationHandler(gravity, 'moAccelGrav'));
-      gravity.start();
-    }
-    
-    let gyroscope = new Gyroscope();
-    gyroscope.addEventListener('reading', e => rotationHandler({
-      alpha: gyroscope.x,
-      beta: gyroscope.y,
-      gamma: gyroscope.z
-    }));
-    gyroscope.start();
-    
-  } else if ('DeviceMotionEvent' in window) {
-    document.getElementById('moApi').innerHTML = 'Device Motion API';
-    
-    var onDeviceMotion = function (eventData) {
-      accelerationHandler(eventData.acceleration, 'moAccel');
-      accelerationHandler(eventData.accelerationIncludingGravity, 'moAccelGrav');
-      rotationHandler(eventData.rotationRate);
-      intervalHandler(eventData.interval);
-    }
-    
-    window.addEventListener('devicemotion', onDeviceMotion, false);
-  } else {
-    document.getElementById('moApi').innerHTML = 'No Accelerometer & Gyroscope API available';
-  }
-  
-  function accelerationHandler(acceleration, targetId) {
-    var info, xyz = "[X, Y, Z]";
-  
-    info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-    info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-    info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
-    document.getElementById(targetId).innerHTML = info;
-  }
-  
-  function rotationHandler(rotation) {
-    var info, xyz = "[X, Y, Z]";
-  
-    info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
-    info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
-    info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
-    document.getElementById("moRotation").innerHTML = info;
-  }
-  
-  function intervalHandler(interval) {
-    document.getElementById("moInterval").innerHTML = interval;
-  }
+  }, 1000);
 });
